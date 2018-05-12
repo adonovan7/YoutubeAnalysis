6b557c35b16baa1ebccfcc78c5c7a07a07a37595
Andie <andiedonovan@yahoo.com>
2018-05-12 00:43:23 -0700

    readme update

--- Goals.md
+++ Goals.md
@@ -1,13 +1,14 @@
 # Goals
 
-1. Output comments to csv along with probabilities (ranked order), predict_proba ****
-2. Compare classifications on test data by eye
-3. Wordcloud
-4. Add new data from additional videos, twitter, UCI database
-5. Dashboard
-	* VADER
+[ ] Output comments to csv along with probabilities (ranked order), predict_proba ****
+[ ] Compare classifications on test data by eye
+[ ] Wordcloud
+[X] Add new data from additional videos, twitter, UCI database
+[X] Dashboard
+[ ] VADER
 
 
+## Goals if time: 
 * Neural Networks on pre-labled text (conditional on data found)
 * AWS to run grid search 
 * research other text sentiment strategies including feature extraction and algorithms 
--- dash/Dashboard6.py
+++ dash/Dashboard6.py
@@ -25,6 +25,7 @@ encoded_image = base64.b64encode(open(image_filename, 'rb').read())
 
 os.chdir(path + 'dash/')
 df = UD.Ratios
+df2 = UD.df
 all_models = UD.all_models
 model_options = ['label_lr', 'label_mnb', 'label_svm', 'label_rf', 'label_knn']
 
@@ -49,24 +50,116 @@ colors = {
     'grey_pal': 'lightgrey'
 }
 
-colors2 = ['#FEBFB3', '#E1396C', '#96D38C', '#D0F9B1']
+# colors2 = ['#FEBFB3', '#E1396C', '#96D38C', '#D0F9B1']
+colors2 = ['#B8F7D4', '#835AF1', '#7FA6EE', '#FEBFB3']
+
+# #835AF1 dark blue
+# #7FA6EE light blue
+# #B8F7D4 green
+
+
+import numpy as np
+df2["com_remv"] = df2["com_remv"].apply(', '.join)
+df2["com_remv"] = df2["com_remv"].str.replace(",","").astype(str)
+
+positive = df2[df2["label"]==1]
+positive = positive["com_remv"]
+negative = df2[df2["label"]==-1]
+negative = negative["com_remv"]
+neutral = df2[df2["label"]==0]
+neutral = neutral["com_remv"]
+
+most_freq_pos = pd.Series(' '.join(positive).lower().split()).value_counts()[:10]
+most_freq_neg = pd.Series(' '.join(negative).lower().split()).value_counts()[:10]
+most_freq_neu = pd.Series(' '.join(neutral).lower().split()).value_counts()[:10]
+
+# word frequency bar plot
+
+Positive = go.Bar(
+            x = most_freq_pos.index,
+            y = most_freq_pos.values,
+            name="Positive",
+            marker=dict(color='#B8F7D4')
+        )
+Neutral = go.Bar(
+            x = most_freq_neu.index,
+            y = most_freq_neu.values,
+            name="Neutral",
+            marker=dict(color='#7FA6EE')
+        )
+Negative = go.Bar(
+            x = most_freq_neg.index,
+            y = most_freq_neg.values,
+            name="Negative",
+            marker=dict(color='#835AF1')
+        )
+
+updatemenus = list([
+            
+            dict(type="buttons",
+                 active=-1,
+                 buttons=list([   
+                    dict(label = 'Positive',
+                         method = 'update',
+                         args = [{'visible': [True, False, False]},
+                                 {'title': 'Positive Comments'}]
+                        ),
+                    dict(label = 'Neutral',
+                         method = 'update',
+                         args = [{'visible': [False, True, False]},
+                                 {'title': 'Neutral Comments'}]
+                        ),
+                    dict(label = 'Negative',
+                         method = 'update',
+                         args = [{'visible': [False, False, True]},
+                                 {'title': 'Negative Comments'}]
+                        ),
+                    dict(label = 'All',
+                         method = 'update',
+                         args = [{'visible': [True, True, True, True]},
+                                 {'title': 'All Comments'}]
+                        )
+                 ]),
+                    pad= {'r': 15, 't': 10}, 
+                )
+        ])
 
 app = dash.Dash()
 
-app.layout = html.Div([
-    html.H1(children='A web application for analyzing comments on Youtube videos', style={
-        'textAlign': 'center',
-        'color': colors['text'], 
-        'padding': 8
-    }),
+'''
+-----------------------------------------------------------------
+'''
 
+'''
+# Video Input Line
     dcc.Input(id='video-input', value='Enter Youtube video URL here', type='text', 
-        style={'width': '600px'}),
+        style={
+                'position': 'relative', 
+                'width': '600px', 
+                'float': 'center', 
+                'display': 'inline-block'},
+                ),
 
-    html.Div(id='video-input-div'),
+    html.Div(id='video-input-div', 
+        style={
+                'position': 'relative', 
+                'width': '600px', 
+                'float': 'center', 
+                'display': 'inline-block'},
+                ),
+'''
 
-    html.H2("Sentiment Ratios by Model"),
+app.layout = html.Div([
 
+# Header
+    html.H1(children='A YouTube Web App', 
+        style={
+            'padding': '10px',
+            'text-align': 'center',
+            'font-size': '40px'}
+        ),
+
+# Pie Chart
     html.Div(
         [
             dcc.Dropdown(
@@ -76,22 +169,45 @@ app.layout = html.Div([
                     'value': i
                 } for i in model_options],
                 value='All Models'),
+            dcc.Graph(id='funnel-graph')
         ],
-        style={'width': '25%',
-               'display': 'inline-block'}),
+            style={
+                'float': 'left',
+                'width': '40.00%',
+                'padding': '10px 10px 10px 0px',
+                'height': '300px'}
+        ),
 
-    dcc.Graph(id='funnel-graph', 
-        style={'display': 'left'}),
+# Bar Chart; Right
+    html.Div([
+        dcc.Graph(
+                id='example-graph',
+                figure={
+                    'data': [Positive, Neutral, Negative],
+                    'layout': go.Layout(title='Most Common Words', barmode='stack', showlegend=True,
+                            updatemenus=updatemenus)
+                        },
+                style={
+                'float': 'right',
+                'width': '55.00%',
+                'padding': '42px 0px 10px 10px',
+                'height': '500px'
+                }
+                )
+            ])
 
-    html.H2("WordCloud"),
-
-    html.Img(src='data:image/png;base64,{}'.format(encoded_image))
+    #html.H2("WordCloud"),
+    #html.Img(src='data:image/png;base64,{}'.format(encoded_image)),
     
     #html.Img(src='/Users/andiedonovan/myProjects/Youtube_Python_Project/AndiesBranch/images/wordcloud.png', 
     #    style={'width': '500px'})
     #html.Img(src='data:image/png;base64,{}'.format(encoded_image))
-    ,
+    
 ])
+
+'''
+-----------------------------------------------------------------
+'''
 
 
 @app.callback(
@@ -106,7 +222,7 @@ def update_graph(Manager):
     trace = go.Pie(labels=["Positive", "Negative","Neutral"], values=values, 
         name='MyModel', hoverinfo='label+percent', 
         textinfo='label + value',textfont=dict(size=20),
-        marker=dict(colors=colors2))
+        marker=dict(colors= colors2))
 
     return {
         'data': [trace],
@@ -116,7 +232,7 @@ def update_graph(Manager):
             )
     }
 
-my_css_url = "https://unpkg.com/normalize.css@5.0.0"
+my_css_url = "https://github.com/adonovan7/YoutubeAnalysis/blob/master/dash/dash.css"
 app.css.append_css({
     "external_url": my_css_url
 })
